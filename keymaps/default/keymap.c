0a1b562a0a3092e4337b174dd0d0e3843c7bde7a
Tze Tseng Soh <144641936+souffle17@users.noreply.github.com>
2024-04-25 18:26:02 -0400

    finish initial map

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -3,6 +3,25 @@
 
 #include QMK_KEYBOARD_H
 
+
+void bootmagic_lite(void) {
+    matrix_scan();
+    wait_ms(DEBOUNCE * 2);
+    matrix_scan();
+    
+    if (matrix_get_row(0) & (1 << 0) 
+        && matrix_get_row(0) & (1 << 1) 
+        && matrix_get_row(0) & (1 << 2) 
+        && matrix_get_row(0) & (1 << 3)
+        && matrix_get_row(0) & (1 << 4)
+        ) {
+        layer_on(0);
+    }
+    else {
+        layer_on(1);
+    }
+}
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * ┌───┬───┬───┬───┬───┐
@@ -14,27 +33,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_ortho_2x6(
         QK_BOOT,    QK_BOOT,    QK_BOOT,    QK_BOOT,    QK_BOOT,
         QK_BOOT,    QK_BOOT,    QK_BOOT,    QK_BOOT,    QK_BOOT
-    )
-    [0] = LAYOUT_ortho_2x6(
+    ),
+    [1] = LAYOUT_ortho_2x6(
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G
     )
-
-    void bootmagic_lite(void) {
-        matrix_scan();
-        wait_ms(DEBOUNCE * 2);
-        matrix_scan();
-        
-        if (matrix_get_row(0) & (1 << 0) 
-            && matrix_get_row(0) & (1 << 1) 
-            && matrix_get_row(0) & (1 << 2) 
-            && matrix_get_row(0) & (1 << 3)
-            && matrix_get_row(0) & (1 << 4)
-            ) {
-            user_config.key_layer = 0;
-        }
-        else {
-            user_config.key_layer = 1;
-        }
-    }
 };
