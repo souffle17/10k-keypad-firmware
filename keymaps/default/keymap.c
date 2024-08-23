5e7efd92e7c4403387a3c8e3fbd6aaad83101a7d
souffle17 <144641936+souffle17@users.noreply.github.com>
2024-08-22 22:06:28 -0700

    clean up voltage processing

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -16,19 +16,18 @@ user_config_t user_config;
 
 bool disable_scan = false;
 
-static uint16_t previous_voltage_1 = 0;
-static uint16_t previous_voltage_2 = 0;
-static bool key_pressed_1 = false; 
-static bool key_pressed_2 = false; 
-static uint16_t buffer = 20;
+static uint16_t buffer = 10;
+
+static uint8_t hall_pins[2] = {F6, F7};
+static uint16_t previous_voltage[2] = {0, 0};
+static bool key_pressed[2] = {false, false};
+static uint16_t hall_keys[2] = {KC_Z, KC_X};
 
 void keyboard_post_init_user(void) {
-    analogReference(ADC_REF_POWER);
     layer_on(user_config.key_layer);
 }
 
 void bootmagic_scan(void) {
-
     matrix_scan();
     wait_ms(DEBOUNCE * 2);
     matrix_scan();
@@ -64,59 +63,59 @@ void bootmagic_scan(void) {
     else if (matrix_get_row(0) & (1 << 3)) {
         layer = 4;
     }
+    else if (matrix_get_row(0) & (1 << 4)) {
+        layer = 5;
+    }
 
     if (user_config.key_layer != layer) {
         user_config.key_layer = layer;
         eeconfig_update_user(user_config.raw); 
+    }
+
+    for (int sensor = 0; sensor < sizeof(hall_pins); sensor++) {
+        previous_voltage[sensor] = analogReadPin(hall_pins[sensor]);
     }
     
 }
 
 // Scary!
 void matrix_scan_user(void) {
+
+    //debug
+    // if (user_config.key_layer == 2) {
+    //     if(matrix_get_row(0) & (1 << 1)) {
+    //         SEND_STRING("\n");
+    //         send_word(analogReadPin(hall_pins[0]));
+    //         SEND_STRING("\n");
+
+    //     }
+    // }
+
+    // only active on layer 1
     if (user_config.key_layer == 1) {
-        uint16_t current_voltage_1 = adc_read(pinToMux(F6));
-        if (current_voltage_1 > previous_voltage_1) {
-            if(!key_pressed_1 && current_voltage_1 > previous_voltage_1 + buffer) {
-                register_code(KC_Z);
-                // SEND_STRING("\n");
-                // send_word(current_voltage_1);
-                // SEND_STRING("\n");
-                key_pressed_1 = true;
+
+        for(int sensor = 0; sensor < sizeof(hall_pins); sensor++) {
+            uint16_t current_voltage = analogReadPin(hall_pins[sensor]);
+            // press the key
+
+            if (!key_pressed[sensor]) {
+                if (current_voltage > previous_voltage[sensor] + buffer) {
+                    register_code(hall_keys[sensor]);
+                    key_pressed[sensor] = true;
+                    previous_voltage[sensor] = current_voltage;
+                }
             }
-            else if(key_pressed_1) {
-                previous_voltage_1 = current_voltage_1;
-            }
-        }
-        else if(current_voltage_1 < previous_voltage_1) {
-            if(key_pressed_1 && current_voltage_1 < previous_voltage_1 - buffer) {
-                unregister_code(KC_Z);
-                key_pressed_1 = false;
-            }
-            else if(!key_pressed_1) {
-                previous_voltage_1 = current_voltage_1;
+
+            //unpress the key
+            else {
+                if (current_voltage < previous_voltage[sensor] - buffer) {
+                    unregister_code(hall_keys[sensor]);
+                    key_pressed[sensor] = false;
+                    previous_voltage[sensor] = current_voltage;
+                }
             }
         }
 
-        uint16_t current_voltage_2 = adc_read(pinToMux(F7));
-        if (current_voltage_2 > previous_voltage_2) {
-            if(!key_pressed_2 && current_voltage_2 > previous_voltage_2 + buffer) {
-                register_code(KC_X);
-                key_pressed_2 = true;
-            }
-            else if(key_pressed_2) {
-                previous_voltage_2 = current_voltage_2;
-            }
-        }
-        else if(current_voltage_2 < previous_voltage_2) {
-            if(key_pressed_2 && current_voltage_2 < previous_voltage_2 - buffer) {
-                unregister_code(KC_X);
-                key_pressed_2 = false;
-            }
-            else if(!key_pressed_2) {
-                previous_voltage_2 = current_voltage_2;
-            }
-        }
     }
 
 }
@@ -148,9 +147,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [4] = LAYOUT_ortho_2x5(
         KC_H,    KC_J,    KC_K,    KC_L,    KC_SEMICOLON,
         KC_N,    KC_M,    KC_COMMA,    KC_DOT,    KC_SLASH
-    ),
-    [5] = LAYOUT_ortho_2x5(
-        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,
-        KC_A,    KC_S,    KC_D,    KC_F,    KC_LEFT_CTRL
     )
 };
